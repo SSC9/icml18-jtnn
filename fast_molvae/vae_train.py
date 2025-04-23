9bcd8e8eb9fe7b9c61993d0c59171608c119d99c
SSC9 <shyamchandra10@gmail.com>
2025-04-23 18:47:25 -0400

    Update vae_train.py to create directory to save checkpoint models

--- fast_molvae/vae_train.py
+++ fast_molvae/vae_train.py
@@ -46,6 +46,9 @@ parser.add_argument('--save_iter', type=int, default=5000)
 args = parser.parse_args()
 print(args)
 
+import os
+os.makedirs(args.save_dir, exist_ok=True)
+
 vocab = [x.strip("\r\n ") for x in open(args.vocab)] 
 vocab = Vocab(vocab)
 
